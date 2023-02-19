e16b25c979f6c9fd0927ac6e62b72a547f69c16b
HatAYE <h.alchaban@student.saedubai.com>
2023-02-19 15:08:36 +0400

    Shooting

---
+++ DOOM_Studio3/Assets/Player/Shooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{                    
+    [SerializeField] float maxShootingDistance;
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxShootingDistance) && Input.GetMouseButtonDown(0))   
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+
+    }
+
+    /*private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, Vector3.forward);
+    }*/
+}

2fd887ce83944f8e7c26510d38cc657336c6c60e
T3NSH11 <m.zaid@student.saedubai.com>
2023-02-23 16:06:54 +0400

    Item Pick script coded

---
+++ DOOM_Studio3/Assets/Player/Controls/PickItem.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItem : MonoBehaviour
+{
+    public float RangeRadius;
+    [Range(0, 360)]
+    public float FOVAngle;
+    public LayerMask ItemMask;
+    public LayerMask WallMask;
+    public Vector3 directionToItem;
+    public GameObject ItemInView;
+    public Transform ItemTransform;
+
+    private void Update()
+    {
+        FieldOfViewCheck();
+
+        if (ItemInView != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(ItemInView);
+            }
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] CollidersInRange = Physics.OverlapSphere(transform.position, RangeRadius, ItemMask);
+
+        if (CollidersInRange.Length != 0)
+        {
+            ItemTransform = CollidersInRange[0].transform;
+
+            directionToItem = (ItemTransform.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToItem) < FOVAngle / 2)
+            {
+                float distanceToItem = Vector3.Distance(transform.position, ItemTransform.position);
+
+                if (!Physics.Raycast(transform.position, directionToItem, distanceToItem, WallMask))
+                {
+                    ItemInView = CollidersInRange[0].gameObject;
+                }
+            }
+        }
+    }
+}

9c50d278d25f29b13a18c5a5b06e1e447e476cb5
T3NSH11 <m.zaid@student.saedubai.com>
2023-03-05 19:34:03 +0400

    Player Movement Updates

--- DOOM_Studio3/Assets/Player/Controls/PlayerMovement.cs
+++ DOOM_Studio3/Assets/Player/Controls/PlayerMovement.cs
@@ -14,9 +14,17 @@ public class PlayerMovement : MonoBehaviour
     public float DashSpeed;
     public bool isDashing;
 
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+
+
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
@@ -26,6 +34,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CameraMovement();
         PlayerRB.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * MoveSpeed, 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void CameraMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
